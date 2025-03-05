4908895c49720c78e287ac5974dc1e82b29eeabc
dangkhoa <dodangkhoaa1.com>
2025-03-05 10:38:16 +0700

    add reload in change language of aac scene, turn off music when open fruitdrop game'

--- IWant.Game/Assets/Scripts/AACApp/Management/AudioManagement.cs
+++ IWant.Game/Assets/Scripts/AACApp/Management/AudioManagement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class AudioManagement : MonoBehaviour
@@ -23,6 +24,24 @@ public class AudioManagement : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(this);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "FruitDrop")
+        {
+            Destroy(gameObject);
         }
     }
 
