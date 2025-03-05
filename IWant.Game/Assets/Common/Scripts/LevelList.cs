c730772890e155b10d1264fa73a2ed3eab0b2938
dangkhoa <dodangkhoaa1.com>
2025-03-05 11:18:56 +0700

    Import DotConnection

---
+++ IWant.Game/Assets/Common/Scripts/LevelList.cs
@@ -0,0 +1,12 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+namespace Connect.Common
+{
+    [CreateAssetMenu(fileName = "Levels", menuName = "SO/AllLevels")]
+    public class LevelList : ScriptableObject
+    {
+        public List<LevelData> Levels;
+    }
+}
+
